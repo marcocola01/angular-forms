61e197ab132f746e0fc25907e0be0c5f4d82757e
colangelo.marco <colangelo.marco@iisgalvanimi.edu.it>
2019-04-08 06:46:23 +0000

    punto 12.1 completo

--- angular-form/src/app/app.module.ts
+++ angular-form/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
+import {   //Importare le funzionalità nella nostra applicazione
+ FormsModule,
+ ReactiveFormsModule
+} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormDemoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  FormsModule,  
+   ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
